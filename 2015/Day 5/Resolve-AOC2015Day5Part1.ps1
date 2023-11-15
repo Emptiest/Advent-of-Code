916b3f8386c19845df4aab73bcefb661c3b34d95
Fullest <68181119+Emptiest@users.noreply.github.com>
2023-11-14 20:24:17 -0600

    Day 5 Part 1

---
+++ 2015/Day 5/Resolve-AOC2015Day5Part1.ps1
@@ -0,0 +1,34 @@
+function Resolve-AOC2015Day5Part1 {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$FilePath
+    )
+
+    $NiceCounter = 0
+
+    foreach($line in Get-Content $FilePath) {
+        $Naughty = $false
+        if($line -match "^.*ab.*|.*cd.*|.*pq.*|.*xy.*$"){           # Rule 3: Do not contain certain strings "ab", "cd", "pq", or "xy"
+            Write-Verbose "$line is naughty"
+            $Naughty = $true
+        }
+        if(-not $Naughty){
+            if($line -notmatch "(\w)\1{1}"){                        # Rule 2: Contain at least one pair
+                Write-Verbose "$line is naughty"
+                $Naughty = $true
+            }
+        }
+        if(-not $Naughty){
+            if($line -notmatch "[aeiou].*[aeiou].*[aeiou]"){        # Rule 1: at least 3 vowels
+                Write-Verbose "$line is naughty"
+                $Naughty = $true
+            }
+        }
+        if(-not $Naughty){
+            Write-Verbose "$line must be nice"
+            $NiceCounter++
+        }
+    }
+    $NiceCounter
+}
