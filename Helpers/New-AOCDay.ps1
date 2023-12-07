208270dfff6a3b48c6872e492d345bce799fe6ff
Fullest <68181119+Emptiest@users.noreply.github.com>
2023-12-06 23:19:20 -0600

    Update New-AOCDay.ps1
    Fixed some issues with the New-AOCDay helper function

--- Helpers/New-AOCDay.ps1
+++ Helpers/New-AOCDay.ps1
@@ -1,19 +1,24 @@
 function New-AOCDay {
     param (
         [int]$Year,
-        [string]$Day
+        [string]$Day,
+        $SessionCookie
     )
 
     $Path = ".\$($Year)\Day $($Day)"
 
-    New-Item -ItemType Directory -Path $Path
+    New-Item -ItemType Directory -Path $Path | Out-Null
+
+    Get-AOCInputFile -SessionCookie $SessionCookie -OutputFile "$Path\input.txt" -Year $Year -Day $Day
 
     if($Day.Length -lt 2){
         $Day = "0" + $Day
     }
 
-    Copy-Item -Path ".\Template\Resolve-AOCYYYYDDPP.ps1" -Destination ".\$($Year)\Day $($Day)\Resolve-AOC$($Year)$($Day)P1.ps1"
+    Copy-Item -Path ".\Template\Resolve-AOCYYYYDDPP.ps1" -Destination "$Path\Resolve-AOC$($Year)$($Day)P1.ps1"
 
     Set-Location $Path
+
+    
 
 }
